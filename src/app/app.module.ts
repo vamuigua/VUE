28f033044ba2548d7f90fb73d7b46e2e2fd73f7d
Victor Allen <vamuigua@gmail.com>
2017-09-16 17:08:51 +0300

    Modified error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import {HttpModule,JsonpModule} from '@angular/http';
+
 
 import { AppComponent }  from './app.component';
 import {MoviesComponent} from './components/movies/movies.component'
 
 
 @NgModule({
-  imports:      [ BrowserModule, HttpModule, JsonpModule ],
+  imports:      [ BrowserModule, HttpModule, JsonpModule, FormsModule ],
   declarations: [ AppComponent, MoviesComponent ],
   bootstrap:    [ AppComponent ]
 })
