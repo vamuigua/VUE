8399927686ae324ef7bd1d436cd34531738edbd0
Victor Allen <vamuigua@gmail.com>
2017-09-16 14:49:15 +0300

    Imported movie service into component

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {MovieService} from '../../services/movie.service';
+
+
 
 @Component({
 	moduleId: module.id,
   selector: 'movies',
   templateUrl: 'movies.component.html',
 })
-export class MoviesComponent  { }
+export class MoviesComponent  { 
+	constructor(private _moviesService: MovieService){
+
+	}
+}
