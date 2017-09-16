9b0b3deb0ef3140edf833422dcd9f11280fdbb5a
Victor Allen <vamuigua@gmail.com>
2017-09-16 14:40:38 +0300

    Added apikey

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -6,6 +6,7 @@ import  'rxjs/Rx';
 export class MovieService{
 	apiKey: string;
 	constructor(private _jsonp:Jsonp){
-		this.apiKey = '';
+		this.apiKey = '9b0149b4dc5fb4460fa59690a962faae';
+		console.log('MovieService  initialized...');
 	}
 }
