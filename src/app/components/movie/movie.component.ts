e6a35ea2e6ab776223ed1800db1f52c694593742
Victor Allen <vamuigua@gmail.com>
2017-09-17 09:51:17 +0300

    set the class MovieComponent for getting a movie

---
+++ src/app/components/movie/movie.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {MovieService} from  '../../services/movie.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'movie',
+  templateUrl: 'movie.component.html',
+})
+
+export class MovieComponent implements OnInit{
+	movie: Object;
+
+	constructor(
+		private router:ActivatedRoute,
+		private _moviesService:MovieService){
+
+	}
+
+	ngOnInit(){
+		this.router.params.subscribe((params) => {
+			let id = params['id'];
+			this._moviesService.getMovie(id).subscribe(movie => {
+				console.log(movie);
+			});
+		});
+	}
+}
