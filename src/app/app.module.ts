ee307efd430e18943233a15fb1a784640790c1cc
Victor Allen <vamuigua@gmail.com>
2017-09-17 09:41:43 +0300

    Import routing and moviecomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {HttpModule,JsonpModule} from '@angular/http';
+import {routing} from './app.routing';
 
 
 import { AppComponent }  from './app.component';
 import {MoviesComponent} from './components/movies/movies.component'
-
+import {MovieComponent} from './components/movie/movie.component'
 
 @NgModule({
-  imports:      [ BrowserModule, HttpModule, JsonpModule, FormsModule ],
-  declarations: [ AppComponent, MoviesComponent ],
+  imports:      [ BrowserModule, HttpModule, JsonpModule, FormsModule, routing ],
+  declarations: [ AppComponent, MoviesComponent, MovieComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
